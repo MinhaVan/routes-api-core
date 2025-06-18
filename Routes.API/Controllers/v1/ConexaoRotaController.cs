89ad8ee0832f4b80c83a1f530f4e199e5d195d44
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-06-17 19:14:43 -0700

    Rota para ConexaoRota

---
+++ Routes.API/Controllers/v1/ConexaoRotaController.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Routes.Domain.Interfaces.Repositories;
+using Routes.Domain.Utils;
+using Routes.Domain.ViewModels;
+using Routes.Domain.ViewModels.WebSocket;
+
+namespace Routes.API.Controllers.v1;
+
+[ApiController]
+[Authorize("Bearer")]
+[Route("v1/[controller]")]
+public class ConexaoRotaController(
+    ILogger<ConexaoRotaController> _logger,
+    IRabbitMqRepository _rabbitMqRepository,
+    IRedisRepository _redisRepository) : BaseController
+{
+    [HttpPost("Localizacao")]
+    public async Task<IActionResult> AdicionarAsync([FromBody] EnviarLocalizacaoWebSocketRequest data)
+    {
+        if (data is null)
+            return ObterRespostaErro("Dados inválidos.");
+
+        var response = new BaseResponse<EnviarLocalizacaoWebSocketResponse>
+        {
+            Data = new EnviarLocalizacaoWebSocketResponse(
+                data.Latitude,
+                data.Longitude,
+                data.RotaId,
+                data.AlunoId,
+                data.Destino.Latitude,
+                data.Destino.Longitude,
+                data.TipoMensagem
+            ),
+            Mensagem = "Localização recebida com sucesso!",
+            Sucesso = true
+        };
+
+        var mensagem = new BaseQueue<EnviarLocalizacaoWebSocketResponse>
+        {
+            Mensagem = response.Data,
+            Retry = 0
+        };
+
+        _rabbitMqRepository.Publish(RabbitMqQueues.EnviarLocalizacao, mensagem, shouldThrowException: false);
+
+        await _redisRepository.SetAsync(ObterRedisKey(data.RotaId), response.Data);
+        return Success();
+    }
+
+    [HttpGet("Responsavel/{responsavelId}/Rota/{rotaId}")]
+    public async Task<IActionResult> ObterLocalizacaoAsync([FromRoute] int responsavelId, [FromRoute] int rotaId)
+    {
+        if (responsavelId <= 0 || rotaId <= 0)
+            return ObterRespostaErro("IDs inválidos para o responsável ou rota.");
+
+        try
+        {
+            var localizacaoCache = await _redisRepository.GetAsync<EnviarLocalizacaoWebSocketResponse>(ObterRedisKey(rotaId));
+            var mensagem = localizacaoCache is null ? "Nenhuma localização disponível no momento." : "Localização recebida com sucesso!";
+
+            return Success(localizacaoCache, mensagem);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Erro ao adicionar o responsável {ResponsavelId} na rota {RotaId}", responsavelId, rotaId);
+            return ObterRespostaErro("Ocorreu um erro ao tentar receber os dados da localização!");
+        }
+    }
+
+    private string ObterRedisKey(int rotaId)
+        => string.Format(KeyRedis.EnviarLocalizacao, rotaId);
+
+    private IActionResult ObterRespostaErro(string mensagem)
+    {
+        return Success(new BaseResponse<EnviarLocalizacaoWebSocketResponse>
+        {
+            Data = null,
+            Mensagem = mensagem,
+            Sucesso = false
+        });
+    }
+}
--- Routes.Domain/Utils/KeyRedis.cs
+++ Routes.Domain/Utils/KeyRedis.cs
@@ -2,7 +2,7 @@ namespace Routes.Domain.Utils;
 
 public static class KeyRedis
 {
-    public const string EnviarLocalizacao = "enviar-localizacao-{0}";
+    public const string EnviarLocalizacao = "enviar-localizacao-rota-{0}";
     public static class Rotas
     {
         public const string Empresa = "rota-empresa-{0}-deletado-{1}";
